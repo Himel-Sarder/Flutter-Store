8e2de60d95512154f1a353f59a820e848d7f47e9
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-02-01 15:43:49 +0600

    Add files via upload

---
+++ Text Middle of the Screen/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+/// ---------------- APP ROOT ----------------
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [WelcomeText(), SizedBox(height: 20)],
+        ),
+      ),
+    );
+  }
+}
+
+class WelcomeText extends StatelessWidget {
+  const WelcomeText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Welcome, This is Himel', style: TextStyle(fontSize: 22));
+  }
+}
