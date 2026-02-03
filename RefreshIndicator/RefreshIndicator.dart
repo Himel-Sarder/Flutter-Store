55f23f6c622100b819f81b44ebef2aa67f3ce3b2
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-02-03 23:55:58 +0600

    Add files via upload

---
+++ RefreshIndicator/RefreshIndicator.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  Future<void> _onRefresh() async {
+    await Future.delayed(const Duration(seconds: 2));
+    debugPrint('Refreshed');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Menu'), backgroundColor: Colors.cyan),
+      body: RefreshIndicator(
+        onRefresh: _onRefresh,
+        child: ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Text(
+                '${index + 1}.',
+                style: const TextStyle(fontSize: 16),
+              ),
+              title: Text('Item ${index + 1}'),
+              trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+              onTap: () {
+                debugPrint('Clicked Item ${index + 1}');
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
